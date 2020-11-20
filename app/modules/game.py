eedc57ae9ba7f0b6ec75514bf4a1b6a7a4f6678d
William Anderson <willjatlas@googlemail.com>
2020-11-20 20:58:33 +0000

    added play RPS method in game

--- app/modules/game.py
+++ app/modules/game.py
@@ -1,6 +1,31 @@
 class Game:
 
-    def __init__(self, player_1, player_2):
-        self.player_1 = player_1
-        self.player_2 = player_2
+    def __init__(self):
+        pass
+
+
+    def player_wins(self, player):
+        return f'{player.name} wins by playing {player.choice}'
+        
+
+    def play_rps(self, player_1, player_2):
+        if player_1.choice == player_2.choice:
+           return None
+        elif player_1.choice == "rock":
+            if player_2.choice == "scissors":
+                self.player_wins(player_1)
+            elif player_2.choice == "paper":
+                self.player_wins(player_2)
+        elif player_1.choice == "paper":
+            if player_2.choice == "rock":
+                self.player_wins(player_1)
+            elif player_2.choice == "scissors":
+                self.player_wins(player_2)
+        elif player_1.choice == "scissors":
+            if player_2.choice == "paper":
+                self.player_wins(player_1)
+            elif player_2.choice == "rock":
+                self.player_wins(player_2)
+        
+         
 

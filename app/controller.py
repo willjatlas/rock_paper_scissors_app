5613c3b16fff98b2d0da75a6dfa6407179742271
William Anderson <willjatlas@googlemail.com>
2020-11-22 18:56:37 +0000

    finished extensions, multiplayer TBA

--- app/controller.py
+++ app/controller.py
@@ -1,8 +1,38 @@
 from app import app 
 from flask import render_template, request, redirect
-
+from app.modules.game import Game
+from app.modules.player import Player
 
 @app.route('/')
 def home():
-    return render_template("base.html")
+    return render_template("home.html", title="Home: ")
+
+@app.route('/<choice_1>/<choice_2>')
+def play_rps(choice_1, choice_2):
+    player_1 = Player("Player 1", choice_1)
+    player_2 = Player("Player 2", choice_2)
+    game = Game(player_1, player_2)
+    winner = game.play_rps()
+    return f'{winner}'
+
+@app.route('/singleplay')
+def single_player():
+    return render_template("singleplay.html", title="Single Player: ")
+
+@app.route('/multiplay')
+def multiplayer():
+    return render_template("multiplay.html", title="Multiplayer: ")
+
+@app.route('/play', methods=["POST"])
+def single_play():
+    player_1 = Player(request.form["player_1_name"], request.form["player_1_choice"])
+    player_2 = Player(None, None)
+    game = Game(player_1, player_2)
+    final_winner = game.play_rps()
+    return redirect (f'/winner/{final_winner}')
+
+# Could use sessions with a key, or make a database, but ¯\_(ツ)_/¯, it works.
+@app.route('/winner/<final_winner>')
+def winner(final_winner):
+    return render_template("winner.html", winner=final_winner, title="WINNER: ")
 
--- app/modules/game.py
+++ app/modules/game.py
@@ -1,17 +1,32 @@
+import random
+
 class Game:
 
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
+        self.choice_list = ["rock", "paper", "scissors"]
 
 
     def player_wins(self, player):
+        """ Helper method to return the winner string """
         return f'{player.name} wins by playing {player.choice}'
-        
+    
+    def check_for_cpu_player(self):
+        """ Check to see if this is a single player game """ 
+        if self.player_2.choice == None:
+            self.generate_cpu_player(self.player_2)
+
+    def generate_cpu_player(self, cpu):
+        """ Make the choice for the computer player """ 
+        cpu.choice = random.choice(self.choice_list)
+        cpu.name = "A (°_o) Definitely Human (°_o) I"
 
     def play_rps(self):
+        self.check_for_cpu_player()
+        """ Method of logic for rps game"""
         if self.player_1.choice == self.player_2.choice:
-            return None
+            return "It's a draw!!!"
         elif self.player_1.choice == "rock":
             if self.player_2.choice == "scissors":
                 return self.player_wins(self.player_1)
